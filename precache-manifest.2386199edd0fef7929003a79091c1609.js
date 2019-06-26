5f25419423f9e18a117dd10357cb859148cdee36
vale75020 <valentinosuma@gmail.com>
2019-06-26 16:40:29 +0200

    Updates

---
+++ precache-manifest.2386199edd0fef7929003a79091c1609.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c225ea681d0d604bb7f063159bd0bbeb",
+    "url": "/redux-examples/index.html"
+  },
+  {
+    "revision": "3c70d74bb92c8209e1e7",
+    "url": "/redux-examples/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a81032b70d0255fafd0f",
+    "url": "/redux-examples/static/js/2.750d86f1.chunk.js"
+  },
+  {
+    "revision": "3c70d74bb92c8209e1e7",
+    "url": "/redux-examples/static/js/main.6d0b63d2.chunk.js"
+  },
+  {
+    "revision": "94385cafc125a9e87ea5",
+    "url": "/redux-examples/static/js/runtime~main.3504d257.js"
+  }
+]);
